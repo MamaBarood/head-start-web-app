dbba650e786dd236df840253e05381d3b00c0b3a
MamaBarood <baroodyk@hotmail.com>
2025-05-01 13:56:50 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from difflib import get_close_matches
 
 # Load data
 def load_data():
-    df = pd.read_excel("HS data for web app 2024.xlsx")
+    df = pd.read_excel("cleaned_hs_data.xlsx")
     columns_needed = [
         "program_name", "state", "head_start_slots", "funding_per_pupil_base",
         "geo_adjusted_funding", "pct_below_100_fpl", "pct_foster_care",
